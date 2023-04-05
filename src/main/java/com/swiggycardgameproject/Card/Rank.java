4956ac973e15cb9f81e34a4bac6415039ec8f872
ashwani8418 <68030476+ashwani8418@users.noreply.github.com>
2023-04-05 11:43:03 +0530

    A separate Card Package created and Rank and suit of the card defined.

---
+++ src/main/java/com/swiggycardgameproject/Card/Rank.java
@@ -0,0 +1,19 @@
+package com.swiggycardgameproject.Card;
+
+public enum Rank {
+    ACE,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+
+}
---
+++ src/main/java/com/swiggycardgameproject/Card/Suit.java
@@ -0,0 +1,8 @@
+package com.swiggycardgameproject.Card;
+
+public enum Suit {
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES
+}
